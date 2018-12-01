1a395d1e49fb31a01bb2929fd5468b33c905e26b
ValerieDobriak <valeria.dobriak@gmail.com>
2018-12-01 10:10:56 +0200

    modify User class for fiting you hibernate requirements

--- src/main/java/ua/nure/kn/dobriak/usermanagement/User.java
+++ src/main/java/ua/nure/kn/dobriak/usermanagement/User.java
@@ -1,17 +1,28 @@
 package ua.nure.kn.dobriak.usermanagement;
 
+import javax.persistence.*;
 import java.time.Period;
 import java.time.LocalDate;
 
+@Entity
+@Table(name="USERS")
 public class User {
-	private String firstName;
-	private String lastName;
-	private LocalDate dateOfBirth;
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@Column(name="first_name")
+	private String firstName;
+
+	@Column(name="last_name")
+	private String lastName;
+
+	@Column(name="birthday")
+	private LocalDate dateOfBirth;
 	
 	public User() {}
-	public User(Long id, String firstName, String lastName, LocalDate dateOfBirth) {
-		this.id = id;
+	public User(String firstName, String lastName, LocalDate dateOfBirth) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.dateOfBirth = dateOfBirth;
@@ -48,6 +59,12 @@ public class User {
 	public Object getAge() {
 		LocalDate todayDate = LocalDate.now();
 		return Period.between(getDateOfBirth(), todayDate).getYears();
+	}
+
+	public void clone(User user) {
+		firstName = user.firstName;
+		lastName = user.lastName;
+		dateOfBirth = user.dateOfBirth;
 	}
 }
 
