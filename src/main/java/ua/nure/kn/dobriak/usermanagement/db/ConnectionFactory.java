ca89d1eb75392517637b9de7e5cc98e0b2ce8253
ValerieDobriak <valeria.dobriak@gmail.com>
2018-12-01 10:02:56 +0200

    swithes from HSQLDB to Hibernate

--- src/main/java/ua/nure/kn/dobriak/usermanagement/db/ConnectionFactory.java
+++
@@ -1,9 +0,0 @@
-package ua.nure.kn.dobriak.usermanagement.db;
-
-import java.sql.Connection;
-
-public interface ConnectionFactory {
-
-	Connection createConnection() throws DatabaseException;
-	
-}
