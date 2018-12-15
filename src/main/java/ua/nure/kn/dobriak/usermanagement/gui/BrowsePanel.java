023928dc2b5664109c868488b27a7d698e61a8b8
ValeriaDobriak <valeriia.dobriak@nure.ua>
2018-12-15 07:26:41 +0200

    add initialize() and getContentPanel() and getBrowsePanel() methods to MainFrame class and create BrowsePanel class

---
+++ src/main/java/ua/nure/kn/dobriak/usermanagement/gui/BrowsePanel.java
@@ -0,0 +1,9 @@
+package ua.nure.kn.dobriak.usermanagement.gui;
+
+import javax.swing.*;
+
+public class BrowsePanel extends JPanel {
+    public BrowsePanel(MainFrame frame) {
+
+    }
+}
--- src/main/java/ua/nure/kn/dobriak/usermanagement/gui/MainFrame.java
+++ src/main/java/ua/nure/kn/dobriak/usermanagement/gui/MainFrame.java
@@ -1,6 +1,45 @@
 package ua.nure.kn.dobriak.usermanagement.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
+    private static final int FRAME_HEIGHT = 600;
+    private static final int FRAME_WIDTH = 800;
+    private JPanel contentPanel;
+    private JPanel browsePanel;
+
+    public MainFrame() {
+        super();
+        initialize();
+    }
+
+    private void initialize() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        this.setTitle("Управление пользователями");
+        this.setContentPane(getContentPanel());
+    }
+
+    private JPanel getContentPanel() {
+        if (contentPanel == null) {
+            contentPanel = new JPanel();
+            contentPanel.setLayout(new BorderLayout());
+            contentPanel.add(getBrowsePanel(), BorderLayout.CENTER);
+        }
+        return contentPanel;
+    }
+
+    private JPanel getBrowsePanel() {
+        if (browsePanel == null) {
+            browsePanel = new BrowsePanel(this);
+        }
+        ((BrowsePanel) browsePanel).initTable();
+        return browsePanel;
+    }
+
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
+    }
 }
